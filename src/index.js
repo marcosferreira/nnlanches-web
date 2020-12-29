b8a5d17ebadf82d1b1adea9116b35fa47c402dbf
Marcos Ferreira <marcosferreira.developer@gmail.com>
2020-12-29 10:16:49 -0300

    initial setting PWA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -9,6 +10,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
